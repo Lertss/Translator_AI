ae9462b20bfcd382da95044742a91af496ab1411
Serhii <lerojserg@gmail.com>
2025-01-23 21:35:37 +0100

    The database configuration has been expanded: the base class of models  and SQL query logging () have been added. Changed requirements.txt

--- app/database.py
+++ app/database.py
@@ -1,10 +1,24 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+# database.py
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
@@ -12,9 +26,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
